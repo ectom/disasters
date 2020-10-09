6e3b8a767c16a38e583f88ca5aaa8552cd8a3bda
Ethan Tom <etom579@gmail.com>
2020-10-09 00:01:04 -0700

    Updates

---
+++ precache-manifest.5abeaeaecd080bcd80e3a699631a161b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e8a555367f6d652b58e653a96b87c9c",
+    "url": "/disasters/index.html"
+  },
+  {
+    "revision": "c7d4ea4dfdf7c2970507",
+    "url": "/disasters/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ddfc35e7276d633222fd",
+    "url": "/disasters/static/js/2.1781eb76.chunk.js"
+  },
+  {
+    "revision": "6567c723c12304429a4042582442ec97",
+    "url": "/disasters/static/js/2.1781eb76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7d4ea4dfdf7c2970507",
+    "url": "/disasters/static/js/main.a5143e98.chunk.js"
+  },
+  {
+    "revision": "1e97d4de5cdfc829dc7c",
+    "url": "/disasters/static/js/runtime-main.8aa8bf93.js"
+  },
+  {
+    "revision": "d1b34539f7fd445a63000ac0d536e55d",
+    "url": "/disasters/static/media/redmarker.d1b34539.svg"
+  }
+]);
