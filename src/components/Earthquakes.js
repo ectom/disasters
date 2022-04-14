c362ed083a18f4a7262df987eeb2708172243c23
Ethan Tom <etom579@gmail.com>
2022-04-14 14:55:42 -0700

    formatting

--- src/components/Earthquakes.js
+++ src/components/Earthquakes.js
@@ -1,197 +1,204 @@
-import React, { Component } from 'react';
-import { Paper, Typography, InputLabel, MenuItem, FormControl, Select, Button } from '@material-ui/core';
-import DateFnsUtils from '@date-io/date-fns';
-import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
-import { searchBody } from '../actions/search';
-import Minimap from './Minimap/Minimap';
-import isSea from '../actions/isSea';
-import { store } from '../store';
+import React, { Component } from "react";
+import {
+  Paper,
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+  Button,
+} from "@material-ui/core";
+import DateFnsUtils from "@date-io/date-fns";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+import { searchBody } from "../actions/search";
+import Minimap from "./Minimap/Minimap";
+import isSea from "../actions/isSea";
+import { store } from "../store";
 
-const { auth, items, filter } = require( '@planet/client/api' );
+const { auth, items, filter } = require("@planet/client/api");
 
 export default class Earthquakes extends Component {
-  constructor( props ) {
-    super( props );
+  constructor(props) {
+    super(props);
     this.d = new Date();
-    
+
     this.state = {
       item_id: undefined,
       item_url: undefined,
       earthquakes: [],
-      dateFrom: new Date( `${this.d.getFullYear()}-${this.twoDigits(this.d.getMonth() + 1)}-${this.twoDigits(this.d.getDate()-1)}T${this.twoDigits(this.d.getHours())}:${this.twoDigits(this.d.getMinutes())}:${this.twoDigits(this.d.getSeconds())}` ),
-      dateTo: new Date( `${this.d.getFullYear()}-${this.twoDigits(this.d.getMonth() + 1)}-${this.twoDigits(this.d.getDate())}T${this.twoDigits(this.d.getHours())}:${this.twoDigits(this.d.getMinutes())}:${this.twoDigits(this.d.getSeconds())}` ),
+      dateFrom: new Date(
+        `${this.d.getFullYear()}-${this.twoDigits(
+          this.d.getMonth() + 1
+        )}-${this.twoDigits(this.d.getDate() - 1)}T${this.twoDigits(
+          this.d.getHours()
+        )}:${this.twoDigits(this.d.getMinutes())}:${this.twoDigits(
+          this.d.getSeconds()
+        )}`
+      ),
+      dateTo: new Date(
+        `${this.d.getFullYear()}-${this.twoDigits(
+          this.d.getMonth() + 1
+        )}-${this.twoDigits(this.d.getDate())}T${this.twoDigits(
+          this.d.getHours()
+        )}:${this.twoDigits(this.d.getMinutes())}:${this.twoDigits(
+          this.d.getSeconds()
+        )}`
+      ),
       magnitude: 5,
     };
     this.key = process.env.REACT_APP_API_KEY;
-    this.search4Band();
     this.getUSGS();
   }
-  
+
   twoDigits(func) {
-    return (func < 10 ? '0' : '') + func;
+    return (func < 10 ? "0" : "") + func;
   }
-  
-  useKey() {
-    auth.setKey( store.getState().api_key );
-  }
-  
-  search4Band() {
-    const geo = {
-      "type": "Polygon",
-      "coordinates": [
-        [
-          [-122.52811431884767, 37.665342132088284],
-          [-122.34683990478516, 37.665342132088284],
-          [-122.34683990478516, 37.81493737606794],
-          [-122.52811431884767, 37.81493737606794],
-          [-122.52811431884767, 37.665342132088284]
-        ]
-      ]
-    };
-    const options = {
-      types: ['PSScene4Band'],
-      filter: filter.geometry( 'geometry', geo ),
-      limit: 10
-    };
-    // this.useKey();
-    items.search( options ).then( response => {
-      console.log( response );
-      const tileThumb = `${response[0]._links.thumbnail}?api_key=${this.key}`;
-      this.setState( {
-        item_id: response[0].id,
-        item_url: tileThumb
-      } );
-    } );
-  }
-  
+
   getDate() {
     let time = this.state.earthquakes[0].time;
-    time = time.split( ' ' );
+    time = time.split(" ");
     let month;
     switch (time[1]) {
-      case 'Jan':
-        month = '01';
+      case "Jan":
+        month = "01";
         break;
-      case 'Feb':
-        month = '02';
+      case "Feb":
+        month = "02";
         break;
-      case 'Mar':
-        month = '03';
+      case "Mar":
+        month = "03";
         break;
-      case 'Apr':
-        month = '04';
+      case "Apr":
+        month = "04";
         break;
-      case 'May':
-        month = '05';
+      case "May":
+        month = "05";
         break;
-      case 'Jun':
-        month = '06';
+      case "Jun":
+        month = "06";
         break;
-      case 'Jul':
-        month = '07';
+      case "Jul":
+        month = "07";
         break;
-      case 'Aug':
-        month = '08';
+      case "Aug":
+        month = "08";
         break;
-      case 'Sept' || 'Sep':
-        month = '09';
+      case "Sept" || "Sep":
+        month = "09";
         break;
-      case 'Oct':
-        month = '10';
+      case "Oct":
+        month = "10";
         break;
-      case 'Nov':
-        month = '11';
+      case "Nov":
+        month = "11";
         break;
       default:
-        month = '12';
+        month = "12";
         break;
     }
-    const date = parseInt( time[2] );
+    const date = parseInt(time[2]);
     const year = time[3];
-    return [`${year}-${month}-${date - 2}T00:00:00.000Z`, `${year}-${month}-${date + 2}T23:59:59.999Z`];
-    // TODO move this url somewhere else. url does not work without item types and ids (supposed to bring user to explorer
-    //  console.log(`https://www.planet.com/explorer/#/center/${this.state.earthquake.point[0]},${this.state.earthquake.point[0]}/dates/${year}-${month}-${date - 2}T00:00:00.000Z..${year}-${month}-${date + 2}T23:59:59.999Z/geometry/POLYGON((${this.state.earthquake.bbox[0][0]}+${this.state.earthquake.bbox[0][1]},${this.state.earthquake.bbox[1][0]}+${this.state.earthquake.bbox[1][1]},${this.state.earthquake.bbox[2][0]}+${this.state.earthquake.bbox[2][1]},${this.state.earthquake.bbox[3][0]}+${this.state.earthquake.bbox[3][1]},${this.state.earthquake.bbox[0][0]}+${this.state.earthquake.bbox[0][1]}))` )
+    return [
+      `${year}-${month}-${date - 2}T00:00:00.000Z`,
+      `${year}-${month}-${date + 2}T23:59:59.999Z`,
+    ];
   }
-  
-  pointToBBOX( lon, lat ) {
-    return [[
-      [lon - 0.2, lat + 0.2],
-      [lon + 0.2, lat + 0.2],
-      [lon - 0.2, lat - 0.2],
-      [lon + 0.2, lat - 0.2],
-      [lon - 0.2, lat + 0.2]
-    ]];
+
+  pointToBBOX(lon, lat) {
+    return [
+      [
+        [lon - 0.2, lat + 0.2],
+        [lon + 0.2, lat + 0.2],
+        [lon - 0.2, lat - 0.2],
+        [lon + 0.2, lat - 0.2],
+        [lon - 0.2, lat + 0.2],
+      ],
+    ];
   }
-  
+
   getUSGS() {
     let { year, month, date } = this.getDateTwoDaysAgo();
-    console.log( 'today', year, month, date );
+    console.log("today", year, month, date);
     const start = `${this.state.dateFrom.getFullYear()}-${this.state.dateFrom.getMonth()}-${this.state.dateFrom.getDate()}`;
     const end = `${this.state.dateTo.getFullYear()}-${this.state.dateTo.getMonth()}-${this.state.dateTo.getDate()}`;
-    
-    fetch( `https://earthquake.usgs.gov/fdsnws/event/1/query?minmagnitude=${this.state.magnitude}&format=geojson&limit=10&starttime=${start}&endtime=${end}`, {
-      // fetch( `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minmagnitude=5&limit=10`, {
-      method: 'GET',
-      mode: 'cors',
-      cache: 'no-cache',
-      credentials: 'same-origin',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      redirect: 'follow',
-      referrerPolicy: 'no-referrer',
-    } ).then( ( res ) => {
-      return res.json();
-    } ).then( res => {
-      console.log( 'USGS', res );
-      const quakes = res.features;
-      if ( quakes === [] ) {
-      
-      } else {
-        let earthquakes = [];
-        for ( let i = 0; i < quakes.length; i++ ) {
-          console.log( 'coord', quakes[i].geometry.coordinates[0], quakes[i].geometry.coordinates[1] );
-          let milliseconds = new Date( quakes[i].properties.time );
-          let earthquake = {          // TODO store image(s) and link to explorer
-            id: i,
-            time: milliseconds.toDateString(),
-            magnitude: quakes[i].properties.mag,
-            title: quakes[i].properties.title,
-            place: quakes[i].properties.place,
-            point: [quakes[i].geometry.coordinates[1], quakes[i].geometry.coordinates[0]],
-            bbox: this.pointToBBOX( quakes[i].geometry.coordinates[0], quakes[i].geometry.coordinates[1] ),
-            type: quakes[i].properties.earthquake
-          };
-          earthquakes.push( earthquake );
-          console.log( 'time', earthquake.time )
-        }
-        this.setState( { earthquakes: earthquakes }, () => {
-          console.log( this.state );
-          this.getExplorerSites();
-        } );
+
+    fetch(
+      `https://earthquake.usgs.gov/fdsnws/event/1/query?minmagnitude=${this.state.magnitude}&format=geojson&limit=10&starttime=${start}&endtime=${end}`,
+      {
+        // fetch( `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minmagnitude=5&limit=10`, {
+        method: "GET",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        redirect: "follow",
+        referrerPolicy: "no-referrer",
       }
-    } )
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        console.log("USGS", res);
+        const quakes = res.features;
+        if (quakes === []) {
+        } else {
+          let earthquakes = [];
+          for (let i = 0; i < quakes.length; i++) {
+            console.log(
+              "coord",
+              quakes[i].geometry.coordinates[0],
+              quakes[i].geometry.coordinates[1]
+            );
+            let milliseconds = new Date(quakes[i].properties.time);
+            let earthquake = {
+              // TODO store image(s) and link to explorer
+              id: i,
+              time: milliseconds.toDateString(),
+              magnitude: quakes[i].properties.mag,
+              title: quakes[i].properties.title,
+              place: quakes[i].properties.place,
+              point: [
+                quakes[i].geometry.coordinates[1],
+                quakes[i].geometry.coordinates[0],
+              ],
+              bbox: this.pointToBBOX(
+                quakes[i].geometry.coordinates[0],
+                quakes[i].geometry.coordinates[1]
+              ),
+              type: quakes[i].properties.earthquake,
+            };
+            earthquakes.push(earthquake);
+            console.log("time", earthquake.time);
+          }
+          this.setState({ earthquakes: earthquakes }, () => {
+            console.log(this.state);
+            this.getExplorerSites();
+          });
+        }
+      });
   }
-  
+
   getExplorerSites() {
     // constructing search body
-    if(this.state.earthquakes.length > 0){
+    if (this.state.earthquakes.length > 0) {
       const geoConfig = {
-        "type": "Polygon",
-        "coordinates": this.pointToBBOX( this.state.earthquakes[0].point[0], this.state.earthquakes[0].point[1] )
+        type: "Polygon",
+        coordinates: this.pointToBBOX(
+          this.state.earthquakes[0].point[0],
+          this.state.earthquakes[0].point[1]
+        ),
       };
       const dateRange = this.getDate();
-      const { search_filter, item_types } = searchBody( geoConfig, dateRange );
-  
-      if(store.getState().isLoggedIn) {
-        this.useKey();
-        items.search( { filter: search_filter, types: item_types } ).then( response => {
-          console.log( 'response', response );
-        } );
-      }
+      const { search_filter, item_types } = searchBody(geoConfig, dateRange);
     }
   }
-  
+
   getDateTwoDaysAgo() {
     let today = new Date();
     let year = today.getFullYear();
@@ -199,56 +206,88 @@ export default class Earthquakes extends Component {
     let date = today.getDate() - 2;
     return { year, month, date };
   }
-  
-  handleDateChange = date => {
-    let newDate = new Date( date );
-    newDate.setDate( newDate.getDate() + 1 );
-    this.setState( { dateFrom: date, dateTo: newDate }, () => {
-      this.getUSGS()
-    } );
+
+  handleDateChange = (date) => {
+    let newDate = new Date(date);
+    newDate.setDate(newDate.getDate() + 1);
+    this.setState({ dateFrom: date, dateTo: newDate }, () => {
+      this.getUSGS();
+    });
   };
-  
-  handleMagnitudeChange = ( event ) => {
-    this.setState( { magnitude: event.target.value }, () => {
-      this.getUSGS()
-    } );
+
+  handleMagnitudeChange = (event) => {
+    this.setState({ magnitude: event.target.value }, () => {
+      this.getUSGS();
+    });
   };
-  
-  Earthquake = ( quake ) => {
-    console.log( quake );
-    const locationType = isSea( quake.point[1], quake.point[0] ) ? 'Sea' : 'Land';
+
+  Earthquake = (quake) => {
+    console.log(quake);
+    const locationType = isSea(quake.point[1], quake.point[0]) ? "Sea" : "Land";
     return (
       <React.Fragment key={quake.id}>
-        <Paper style={{ padding: '2%', paddingBottom: '-2%', margin: '5%', overflow: 'hidden', minWidth: '750px' }}>
-          <Minimap key={`${quake.point[1]}/${quake.point[0]}`} lat={quake.point[1]} long={quake.point[0]} zoom={3}/>
-          <div style={{ display: 'inline-block', marginLeft: '5%', verticalAlign: 'top' }}>
-            <Typography variant={'h5'}>{quake.title}</Typography>
-            <Typography variant={'body1'}><strong>Magnitude:</strong> {quake.magnitude}</Typography>
-            <Typography variant={'body1'}><strong>Place:</strong> {quake.place}</Typography>
-            <Typography variant={'body1'}><strong>Time of earthquake:</strong> {quake.time}</Typography>
-            <Typography variant={'body1'}><strong>Coordinates of
-              earthquake:</strong> {quake.point[0]}°N, {quake.point[1]}°E</Typography>
-            <Typography variant={'body1'}><strong>Land or Sea:</strong> {locationType}</Typography>
+        <Paper
+          style={{
+            padding: "2%",
+            paddingBottom: "-2%",
+            margin: "5%",
+            overflow: "hidden",
+            minWidth: "750px",
+          }}
+        >
+          <Minimap
+            key={`${quake.point[1]}/${quake.point[0]}`}
+            lat={quake.point[1]}
+            long={quake.point[0]}
+            zoom={3}
+          />
+          <div
+            style={{
+              display: "inline-block",
+              marginLeft: "5%",
+              verticalAlign: "top",
+            }}
+          >
+            <Typography variant={"h5"}>{quake.title}</Typography>
+            <Typography variant={"body1"}>
+              <strong>Magnitude:</strong> {quake.magnitude}
+            </Typography>
+            <Typography variant={"body1"}>
+              <strong>Place:</strong> {quake.place}
+            </Typography>
+            <Typography variant={"body1"}>
+              <strong>Time of earthquake:</strong> {quake.time}
+            </Typography>
+            <Typography variant={"body1"}>
+              <strong>Coordinates of earthquake:</strong> {quake.point[0]}°N,{" "}
+              {quake.point[1]}°E
+            </Typography>
+            <Typography variant={"body1"}>
+              <strong>Land or Sea:</strong> {locationType}
+            </Typography>
           </div>
         </Paper>
       </React.Fragment>
-    )
+    );
   };
-  
+
   render() {
-    
-    let earthquakes = this.state.earthquakes.map( quake => this.Earthquake( quake ) );
+    let earthquakes = this.state.earthquakes.map((quake) =>
+      this.Earthquake(quake)
+    );
     if (earthquakes.length === 0) {
-      earthquakes = <p style={{textAlign: 'center', marginTop: '20%'}}>No Results</p>
+      earthquakes = (
+        <p style={{ textAlign: "center", marginTop: "20%" }}>No Results</p>
+      );
     }
-    
+
     return (
       <>
         <MuiPickersUtilsProvider utils={DateFnsUtils}>
-          <div style={{height: '64px'}} />
+          <div style={{ height: "64px" }} />
           <div
             style={{
-              margin: '0% 5% 0% 5%'
+              margin: "0% 5% 0% 5%",
             }}
           >
             <KeyboardDatePicker
@@ -260,11 +299,14 @@ export default class Earthquakes extends Component {
               value={this.state.dateFrom}
               onChange={this.handleDateChange}
               KeyboardButtonProps={{
-                'aria-label': 'change date',
+                "aria-label": "change date",
               }}
             />
-            <FormControl style={{ margin: '15px' }}>
-              <InputLabel shrink id="demo-simple-select-placeholder-label-label">
+            <FormControl style={{ margin: "15px" }}>
+              <InputLabel
+                shrink
+                id="demo-simple-select-placeholder-label-label"
+              >
                 Magnitude
               </InputLabel>
               <Select
@@ -292,6 +334,6 @@ export default class Earthquakes extends Component {
           {earthquakes}
         </MuiPickersUtilsProvider>
       </>
-    )
+    );
   }
 }
